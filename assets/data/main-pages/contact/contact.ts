e6da027d5bea592dd34858e2514263d58ce28664
TPhongWork <ttfongwork@gmail.com>
2024-11-23 02:16:09 +0700

    build web

---
+++ assets/data/main-pages/contact/contact.ts
@@ -0,0 +1,35 @@
+const ContactData={
+    title: 'LIÊN HỆ',
+    content: 'Liên hệ trực tiếp qua hotline , địa chỉ email công ty để được tư vấn và giải đáp', 
+    address:'Yên Viên , H. Gia Lâm , Tp. Hà nội',
+    phoneNumber: '0363499815',
+    email:'gosach90@gmail.com',
+    addressTitle: 'Địa chỉ',
+    phoneNumberTitle: 'Số điện thoại',
+    emailTitle: 'Email',
+}
+const SaleTeam = {
+    title:'Chat với đội ngũ Sales',
+    content:'~ Hãy liên hệ với đội ngũ sales của chúng tôi để được tư vấn và giải đáp thắc mắc cho bạn ~',
+    listSales:[
+        {
+            name:'Mr. Lê Trung Thành',
+            imgUrl:'/assets/img/pages/teamsales/teamsale-1.jpg',
+            phoneNumber: '0363499815',
+            email: 'letrungthanh@gmail.com'
+        },
+        {
+            name:'Mr. Lê Trung Bình',
+            imgUrl:'/assets/img/pages/teamsales/teamsale-1.jpg',
+            phoneNumber: '0363499815',
+            email: 'letrungthanh@gmail.com'
+        },
+        {
+            name:'Mr. Lê Trung Điểm',
+            imgUrl:'/assets/img/pages/teamsales/teamsale-1.jpg',
+            phoneNumber: '0363499815',
+            email: 'letrungthanh@gmail.com'
+        }
+    ]
+}
+export {ContactData,SaleTeam};
